5af470e765e3d11a3111e7c01971842f42c78f80
mariojacinto118 <marioantoniodomingos118@gmail.com>
2026-02-22 10:19:55 +0100

    algumas

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import { ArrowRight, Star, Wind, Waves, Utensils } from 'lucide-react';
 import { useLanguage } from '../context/LanguageContext';
 import { useData } from '../context/DataContext';
-import RoomCard from '../components/RoomCard';
+import { RoomCard } from '../components/RoomCard';
 import { getStorageUrl } from '../supabase';
 
 const Home: React.FC = () => {
